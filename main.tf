5babd56caec8aa9e3be8260efc31bc782d2096b4
Mandar Nakhe <mandar.nakhe@gmail.com>
2021-11-09 12:57:41 +0530

    terraform file11

--- main.tf
+++ main.tf
@@ -31,3 +31,17 @@ resource "google_compute_instance" "myvm1" {
                 }
         }
 }
+
+resource "google_compute_instance" "myvm2" {
+        name = "mythirdvm"
+        machine_type = "f1-micro"
+        zone = "us-central1-a"
+        network_interface {
+                network = "default"
+        }
+        boot_disk {
+                initialize_params {
+                        image = "debian-cloud/debian-10"
+                }
+        }
+}
